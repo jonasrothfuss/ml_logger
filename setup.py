9db50b24af8c5b29f229346325825328711f1aed
jonasrothfuss <jonas.rothfuss@gmx.de>
2019-03-05 12:26:16 +0100

    remove vis-server from steup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(name="ml_logger",
       author="Ge Yang",
       author_email="yangge1987@gmail.com",
       license=None,
-      keywords=["ml_logger", "vis_server" "logging", "debug", "debugging"],
+      keywords=["ml_logger", "logging", "debug", "debugging"],
       classifiers=[
           "Development Status :: 4 - Beta",
           "Intended Audience :: Science/Research",
           "Programming Language :: Python :: 3"
       ],
-      packages=["ml_logger", "vis_server"],
+      packages=["ml_logger"],
       install_requires=["typing", "numpy", "termcolor", "params_proto", "cloudpickle", "japronto",
                         'dill', 'ruamel.yaml']
       )
